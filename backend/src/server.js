423bf73487d0280b3ac38614b2862b785b0f0404
AkashDavidKumar <akashdavidkumar77@gmail.com>
2026-02-15 11:21:24 +0530

    adding * on server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // 👇 Express 5 Safe Fallback
-  app.use((req, res) => {
+  app.use("*", (req, res) => {
     res.sendFile(
       path.join(__dirname, "../frontend", "dist", "index.html")
     );
